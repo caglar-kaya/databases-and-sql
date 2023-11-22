02201030f221bd86d9a721c41341be0c320e39b3
Caglar Kaya <c.kaya@intermax.nl>
2023-11-22 12:02:11 +0100

    notes: sub-queries and implicit joins

---
+++ week-03/02-Built-in Database Functions, Multiple Tables, and Sub-queries/03-Working with Multiple Tables.sql
@@ -0,0 +1,50 @@
+SELECT * 
+FROM EMPLOYEES 
+WHERE JOB_ID IN (SELECT JOB_IDENT FROM JOBS);
+
+SELECT JOB_TITLE, MIN_SALARY, MAX_SALARY, JOB_IDENT
+FROM JOBS
+WHERE JOB_IDENT IN (select JOB_ID from EMPLOYEES where SALARY > 70000 );
+
+SELECT *
+FROM EMPLOYEES, JOBS
+WHERE EMPLOYEES.JOB_ID = JOBS.JOB_IDENT;
+
+SELECT *
+FROM EMPLOYEES E, JOBS J
+WHERE E.JOB_ID = J.JOB_IDENT;
+
+SELECT EMP_ID,F_NAME,L_NAME, JOB_TITLE
+FROM EMPLOYEES E, JOBS J
+WHERE E.JOB_ID = J.JOB_IDENT;
+
+SELECT E.EMP_ID, E.F_NAME, E.L_NAME, J.JOB_TITLE
+FROM EMPLOYEES E, JOBS J
+WHERE E.JOB_ID = J.JOB_IDENT;
+
+-- Practice problems
+-- 1. Retrieve only the list of employees whose JOB_TITLE is Jr. Designer.
+    -- a. Using sub-queries
+    SELECT *
+    FROM EMPLOYEES
+    WHERE JOB_ID IN (SELECT JOB_IDENT
+                    FROM JOBS
+                    WHERE JOB_TITLE= 'Jr. Designer');
+    
+    -- b. Using Implicit Joins
+    SELECT *
+    FROM EMPLOYEES E, JOBS J
+    WHERE E.JOB_ID = J.JOB_IDENT AND J.JOB_TITLE= 'Jr. Designer';
+
+-- 2. Retrieve JOB information and a list of employees whose birth year is after 1976.
+    -- a. Using sub-queries 
+    SELECT JOB_TITLE, MIN_SALARY, MAX_SALARY, JOB_IDENT
+    FROM JOBS
+    WHERE JOB_IDENT IN (SELECT JOB_ID
+                        FROM EMPLOYEES
+                        WHERE YEAR(B_DATE)>1976 );
+
+    -- b. Using Implicit Joins
+    SELECT J.JOB_TITLE, J.MIN_SALARY, J.MAX_SALARY, J.JOB_IDENT
+    FROM JOBS J, EMPLOYEES E
+    WHERE E.JOB_ID = J.JOB_IDENT AND YEAR(E.B_DATE)>1976;
